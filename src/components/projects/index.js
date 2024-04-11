e30139745131d1d76fe6e8bd50c188e4a6dbf1d9
a_verma <82448727+ashishverma94@users.noreply.github.com>
2024-04-11 16:24:47 +0530

    added projects

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -6,22 +6,16 @@ const Projects = () => {
     return (
         <div className='proclass'>
             <h1>My Projects</h1>
-            
                 <ul>
-                    <a href='https://self-driving-car-mu.vercel.app/' target='_blank'><li> Self Driving Car </li></a>
-                    <a href = 'https://image-generator-app-7i7d.vercel.app/' target='_blank'><li> Drawing Pad </li></a>
-                    <a href = 'https://teach-nook-weather-app.vercel.app/' target = '_blank'><li> Weather App</li></a>
                     <a href = 'https://movie-next-project.vercel.app/' target='_blank'><li> Movies App</li></a>
-                    <li> Book Store </li>
-
+                    <a href = 'https://tuf-assignment-rouge.vercel.app/' target = '_blank'><li> Coding Compiler</li></a>
+                    <a href='https://levitation-invoice-generator.vercel.app/login' target='_blank'><li> Invoice Generator App </li></a>
+                    <a href = 'https://blue-dollar-4mqh.vercel.app/' target='_blank'><li> Blue Dollar ( Web Scrapping ) </li></a>
+                    <a href = 'https://next-ai-chatbot-woad.vercel.app/' target = '_blank'><li>ChatBot (ChatGPT )</li></a>
+                    <a href =  'https://zepto-pick-fruit.vercel.app/' target = '_blank'><li>Pic Fruit</li></a>
+                    <a href =  'https://affiliate-project.vercel.app/' target = '_blank'><li>Update Courses ( react-router-dom)</li></a>
                 </ul>
-
-                
-
-               
-
             </div>
-
     )
 }
 
